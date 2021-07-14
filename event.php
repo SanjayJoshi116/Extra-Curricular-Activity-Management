5a900ec600b925656e5167fe67def18ddfaf21a7
saji37 <sajiththomas162631@gmail.com>
2021-07-14 18:27:23 +0530

    events
    viewing upcoming events

--- event.php
+++ event.php
@@ -11,32 +11,43 @@ include("header.php");
           Events
         </h3>
         <p>
-          Upcoming Education Events to feed your brain.
+          Upcoming Events
         </p>
       </div>
+      <?php
+		    $sqlview = "SELECT * FROM  event ";
+		    $qsqlview = mysqli_query($con,$sqlview);
+	    	while($rsview = mysqli_fetch_array($qsqlview))
+		    {
+          ?>
       <div class="event_container">
         <div class="box">
           <div class="img-box">
-            <img src="images/event-img.jpg" alt="" />
+           <h5>
+		  <?php
+      $imge=$rsview['event_banner'];
+      echo '<img src="imgbanner/' .$imge .'" width="150" height="150">';
+      ?>
+		   </h5>
           </div>
           <div class="detail-box">
             <h4>
-              Education Events 2021
+            <?php echo "{$rsview['event_title']}";?>
             </h4>
             <h6>
-              8:00 AM - 5:00 PM VENICE, ITALY
+              <?php echo "{$rsview['event_venue']}";?>
             </h6>
           </div>
           <div class="date-box">
             <h3>
-              <span>
-                15
-              </span>
-              March
+            <?php echo "{$rsview['event_date_time']}";?>
             </h3>
           </div>
         </div>
-        <div class="box">
+        <?php
+      }
+        ?>
+       <?php /*<div class="box">
           <div class="img-box">
             <img src="images/event-img.jpg" alt="" />
           </div>
@@ -56,8 +67,9 @@ include("header.php");
               February
             </h3>
           </div>
-        </div>
-      </div>
+        </div>\
+	</div>*/?>
+     
       <div class="btn-box">
         <a href="">
           Read More
