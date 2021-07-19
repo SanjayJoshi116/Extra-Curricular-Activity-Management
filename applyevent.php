64c96f1c42c78aed8a87d37852d94eea7ecb915f
SanjayJoshi116 <86725113+SanjayJoshi116@users.noreply.github.com>
2021-07-19 15:10:14 +0530

    Update applyevent.php

--- applyevent.php
+++ applyevent.php
@@ -1,12 +1,13 @@
 <?php
 include("header.php");
-/*if(!isset($_SESSION['student_id']))
+if(!isset($_SESSION['student_id']))
 {
 	echo "<script>window.location='login.php';</script>";
-}*/
+}
 if(isset($_POST['submit']))
 {
-	$sql = "INSERT INTO event_participation() VALUES('$_POST[event_id]','$_SESSION[student_id]','$_POST[apply_dt_tim]')";
+	$apply_dt_tim=$_POST[date('Y-m-d H:i:s')];
+	$sql = "INSERT INTO event_participation() VALUES('$_POST[event_id]','$_SESSION[student_id]',$apply_dt_tim')";
 	$qsql = mysqli_query($con,$sql);
 	echo mysqli_error($con);
 	if(mysqli_affected_rows($con)==1)
@@ -38,7 +39,7 @@ if(isset($_POST['submit']))
         <div class="col-md-6">
           <div class="contact-form">
             <h5>
-              Kindly verify the details 
+              Kindly verify the details to apply
             </h5>
             <form action="" method="post" name="frmevent_apply" id="frmevent_apply">
 			
@@ -56,46 +57,6 @@ if(isset($_POST['submit']))
 		}
 		?>
 	</select>
-              </div>
-			  
-              <div>
-				<label class="labelproperty">Event Title</label>
-                <input type="text" name="event_title" id="event_title" placeholder="Enter Event Title" />
-              </div>
-			  
-              <div>
-                <label class="labelproperty">Event Description</label>
-				<textarea name="event_description" id="event_description" class="form-control" placeholder="Enter Event Description"></textarea>
-              </div>
-			  
-              <div>
-				<label class="labelproperty">Event Rules</label>
-				<textarea name="event_rules" id="event_rules" class="form-control" placeholder="Enter Event Rules"></textarea>
-              </div>
-			  
-              <div>
-				<label class="labelproperty">Event Banner</label>
-                <input type="file" name="event_banner" id="event_banner" placeholder="Enter Event Banner" />
-              </div>
-			  
-			  
-              <div>
-				<label class="labelproperty">Department</label>
-                <select name="department_id" id="department_id" class="form-control" >
-					<option value="">All Department</option>
-		<?php
-		$sqldepartment = "SELECT * FROM department WHERE department_status='Active'";
-		$qsqldepartment = mysqli_query($con,$sqldepartment);
-		echo mysqli_error($con);
-		while($rsdepartment = mysqli_fetch_array($qsqldepartment))
-		{
-			echo "<option value='$rsdepartment[department_id]'>$rsdepartment[department]</option>";
-		}
-		?>
-				</select>
-              </div>
-			  
-
 			  
 			  
 			  
