6af6ccfd9e79e4c2b5c9834c420991ade340ec69
SanjayJoshi116 <86725113+SanjayJoshi116@users.noreply.github.com>
2021-09-18 11:03:35 +0530

    application history

--- completed-event.php
+++ completed-event.php
@@ -11,11 +11,11 @@ include("header.php");
           Events
         </h3>
         <p>
-          Upcoming Events
+          Completed Events
         </p>
       </div>
       <?php
-		    $sqlview = "SELECT * FROM  event where event_date_time<CURDATE()";
+		    $sqlview = "SELECT * FROM  event where event_date_time < CURDATE()";
 		    $qsqlview = mysqli_query($con,$sqlview);
 	    	while($rsview = mysqli_fetch_array($qsqlview))
 		    {
@@ -40,9 +40,13 @@ include("header.php");
           </div>
           <div class="date-box">
             <h3>
-            <?php echo "{$rsview['event_date_time']}";?>
+            <?php echo date("d-M-Y",strtotime($rsview['event_date_time']));?>
             </h3>
           </div>
+		  <div>
+		  <a href="event_result_report.php?event_id=<?php echo $rsview['event_id']; ?>" class="btn btn-success">View Result</a>
+		  (<?php echo $rsview['event_participation_type'];?> Event)
+		  </div>
         </div>
         <?php
       }
---
+++ view_application_history.php
@@ -0,0 +1,62 @@
+<?php
+include "header.php";
+if(!isset($_SESSION['student_id']))
+{
+	echo "<script>window.location='login.php';</script>";
+}
+?>
+</div>
+
+<section class="event_section layout_padding">
+    <div class="container">
+      <div class="heading_container">
+        <h3>
+         Application History
+        </h3>
+        <p>
+         View all event applications
+        </p>
+      </div>
+      <div class="event_container">
+        <div class="">
+		<table id="datatableplugin" class="table table-bordered">
+	<thead>
+		<tr>
+			<th>Event Title</th>
+			<th>Partcipation Type</th>
+			<th>Department</th>
+			<th>Participation Status</th>
+			<th>Winning Position</th>
+			<th>Points</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php
+		$sql = "SELECT event_result_status.*,event.*,department.department,event_participation.event_participation_status FROM event_result_status LEFT JOIN event ON event.event_id = event_result_status.event_id LEFT JOIN department ON department.department_id = event.department_id LEFT JOIN event_participation ON event_participation.event_id = event_result_status.event_id";
+		$qsql = mysqli_query($con,$sql);
+		echo mysqli_error($con);	
+		while($rsview = mysqli_fetch_array($qsql))
+		{
+			
+			echo 
+			"<tr>
+				  <td>$rsview[event_title]
+				  <td>$rsview[event_participation_type]</td>
+				  <td>$rsview[department]</td>
+				  <td>$rsview[event_participation_status]</td>
+				  <td>$rsview[winning_position]</td>
+				  <td>$rsview[point]</td>
+			</tr>";
+		}
+	?>
+	</tbody>
+	</table>
+   </div>	
+      </div>
+    </div>
+  </section>
+
+		
+<?php
+include "footer.php";
+?>
