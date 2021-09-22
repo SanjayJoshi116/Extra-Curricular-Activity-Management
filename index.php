0a183ddd51bbc096f7ab61fe76b8d0a0f82c622a
ThejasPC <tejasgowda1132@gmail.com>
2021-09-22 11:53:23 +0530

    index
    homepage changes

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 include("header.php");
 include("slider.php");
 ?>
+<br>
   </div>
-  <!-- special section -->
+<!-- special section -->
 
   <section class="special_section">
     <div class="container">
@@ -91,126 +92,9 @@ include("slider.php");
 
   <!-- end about section -->
 
-  <!-- course section -->
 
-  <section class="course_section layout_padding-bottom">
-    <div class="side_img">
-      <img src="images/side-img.png" alt="" />
-    </div>
-    <div class="container">
-      <div class="heading_container">
-        <h3>
-          POPULAR COURSES
-        </h3>
-        <p>
-          It is a long established fact that a reader will be distracted
-        </p>
-      </div>
-      <div class="course_container">
-        <div class="course_content">
-          <div class="box">
-            <img src="images/c-1.jpg" alt="" />
-            <a href="" class="">
-              <img src="images/link.png" alt="" />
-            </a>
-            <h5>
-              LMS <br />
-              Content
-            </h5>
-          </div>
-          <div class="box">
-            <img src="images/c-2.jpg" alt="" />
-            <a href="" class="">
-              <img src="images/link.png" alt="" />
-            </a>
-            <h5>
-              From <br />
-              Zero to Hero
-            </h5>
-          </div>
-        </div>
-        <div class="course_content">
-          <div class="box">
-            <img src="images/c-3.jpg" alt="" />
-            <a href="" class="">
-              <img src="images/link.png" alt="" />
-            </a>
-            <h5>
-              Learn <br />
-              Python – Interactive
-            </h5>
-          </div>
-          <div class="box">
-            <img src="images/c-4.jpg" alt="" />
-            <a href="" class="">
-              <img src="images/link.png" alt="" />
-            </a>
-            <h5>
-              Your <br />
-              Complete Guide
-            </h5>
-          </div>
-          <div class="box">
-            <img src="images/c-5.jpg" alt="" />
-            <a href="" class="">
-              <img src="images/link.png" alt="" />
-            </a>
-            <h5>
-              Photography
-            </h5>
-          </div>
-        </div>
-      </div>
-      <div class="btn-box">
-        <a href="">
-          Read More
-        </a>
-      </div>
-    </div>
-  </section>
 
-  <!-- end course section -->
-
-  <!-- login section -->
-
-  <section class="login_section layout_padding">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="detail-box">
-            <h3>
-              GET ONLINE COURSES FOR FREE
-            </h3>
-            <p>
-              Create your free account now and get immediate access to 100s of
-              online courses
-            </p>
-            <a href="">
-              REGISTER NOW
-            </a>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="login_form">
-            <h5>
-              Login Now
-            </h5>
-            <form action="">
-              <div>
-                <input type="email" placeholder="Email " />
-              </div>
-              <div>
-                <input type="password" placeholder="Password" />
-              </div>
-              <button type="submit">Login</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- end login section -->
+  
 
   <!-- event section -->
   <section class="event_section layout_padding">
