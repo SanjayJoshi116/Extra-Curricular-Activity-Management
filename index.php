80828be77ef75a5708945d1abfdc6face825abe6
saji37 <sajiththomas162631@gmail.com>
2021-09-23 11:12:34 +0530

    chnges

--- index.php
+++ index.php
@@ -90,6 +90,7 @@ include("slider.php");
   </section>
 
   <!-- end about section -->
+<<<<<<< Updated upstream
 
   <!-- course section -->
 
@@ -97,6 +98,190 @@ include("slider.php");
     <div class="side_img">
       <img src="images/side-img.png" alt="" />
     </div>
+=======
+</div>
+<!-- Quotes -->
+<article class="article">
+    <blockquote>
+    <p>When I was a teenager, I began to settle into school because I'd discovered the extracurricular activities that interested me: music and theater.</p>
+    <cite><span class="cite-last-name">Morgan Freeman</span></cite>
+    <div class="blockquote-author-image" style="--image: url('images/morganfreeman.jfif')"></div>
+  </blockquote>
+</article>
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Vollkorn:ital,wght@0,600;1,500&display=swap');
+
+:root {
+  --type-body: Open Sans, Helvetica, Arial, sans-serif;
+  --type-quote: Vollkorn;
+  --quote-image-width: 140px;
+  --border-rad: 7px;
+  --accent-color: hsl(322deg 85% 65%);
+  --quote-bg: hsl(0 0% 97%);
+}
+
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+a {
+  text-decoration: none;
+  color: var(--accent-color);
+}
+
+a:hover,
+a:focus {
+  text-decoration: underline;
+}
+
+body {
+  margin: 15px 10px;
+  font-family: var(--type-body);
+  color: hsl(0 0% 15%);
+}
+
+.article {
+  max-width: 900px;
+  margin: auto;
+}
+
+h1 {
+  width: max-content;
+  margin: 0 auto 1.8em;
+  box-shadow:
+    0 2px 0 var(--accent-color),
+    0 4px 0 white,
+    0 6px 0 var(--accent-color);
+  font: 1.4rem var(--type-quote);
+  color: hsl(0 0% 25%);
+}
+
+
+blockquote {
+  position: relative;
+  margin: 40px 0;
+  padding: 1.6em 2.4em .7em calc(1.4em + var(--quote-image-width));
+  font: italic 1.2rem var(--type-quote);
+  background: var(--quote-bg) no-repeat left / var(--quote-image-width);
+  border-radius: var(--border-rad);
+  border: 2px solid white;
+  box-shadow: 2px 2px 4px hsl(0 0% 0% / 20%);
+  text-indent: 1.6em;
+}
+
+@media (min-width: 768px) {
+  blockquote {
+    margin: 40px 60px;
+  }
+}
+
+blockquote::before {
+  content: "";
+  pointer-events: none;
+  position: absolute;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  border-radius: var(--border-rad);
+  box-shadow:
+    inset -2px -2px 1px hsl(0 0% 100%),
+    inset 2px 2px 4px hsl(0 0% 0% / 20%);
+}
+
+blockquote::after {
+  content: "❝";
+  position: absolute;
+  z-index: 1;
+  left: 50%;
+  top: -2px;
+  transform: translate(-50%, -50%);
+  width: 1.3em;
+  height: 1.3em;
+  background: white;
+  box-shadow: 0 4px 5px -1px hsla(0 0% 0% / 20%);
+  border-radius: 999px;
+  display: grid;
+  place-content: center;
+  padding-top: .5em;
+  color: var(--accent-color);
+  font-size: 36px;
+  font-style: normal;
+  text-indent: 0;
+}
+
+.blockquote-author-image {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: var(--quote-image-width);
+  height: 100%;
+  opacity: 0.75;
+  background: var(--accent-color) var(--image) no-repeat center / cover;
+  background-blend-mode: hard-light;
+  border-top-left-radius: var(--border-rad);
+  border-bottom-left-radius: var(--border-rad);
+}
+
+cite {
+  display: block;
+  margin-top: 30px;
+  text-indent: 0;
+  text-align: center;
+  font: bold .9rem var(--type-body);
+  text-transform: uppercase;
+  color: hsl(0 0% 20%);
+}
+
+@media (min-width: 768px) {
+  cite {
+    margin-left: calc(1rem - var(--quote-image-width));
+  }
+}
+
+.cite-last-name {
+  background: var(--accent-color);
+  color: var(--quote-bg);
+}
+.controls {
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  font-size: 0.8em;
+  opacity: 0.7;
+  transition: .2s;
+}
+
+.controls:hover,
+.controls:focus {
+  opacity: 1;
+}
+
+.controls label {
+  font-weight: bold;
+  text-transform: lowercase;
+}
+
+.controls input {
+  display: block;
+  width: 100%;
+  border: 0;
+  outline: none;
+  padding: 0;
+}
+</style>
+<script>
+  document.querySelector('.js-accent-color').addEventListener('change', (e) => {
+  document.documentElement.style.setProperty('--accent-color', e.target.value)
+})
+</script>
+  <!-- event section -->
+  <section class="event_section layout_padding">
+>>>>>>> Stashed changes
     <div class="container">
       <div class="heading_container">
         <h3>
@@ -269,6 +454,7 @@ include("slider.php");
       </div>
     </div>
   </section>
+<<<<<<< Updated upstream
 
   <!-- end event section -->
 
@@ -322,6 +508,8 @@ include("slider.php");
   </section>
 
   <!-- end contact section -->
+=======
+>>>>>>> Stashed changes
 <?php
 include("footer.php");
 ?>
