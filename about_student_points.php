efaae35894cb9769a5ff601a0b941035abfa99f2
saji37 <sajiththomas162631@gmail.com>
2021-09-24 11:07:48 +0530

    Create about_student_points.php

---
+++ about_student_points.php
@@ -0,0 +1,36 @@
+<?php
+include("header.php");
+?>
+</div>
+
+  <!-- about section -->
+  <section class="about_section layout_padding">
+    <div class="side_img">
+      <img src="images/side-img.png" alt="" />
+    </div>
+    <div class="container">
+      <div class="row">
+          <div class="detail-box">
+            <div class="heading_container">
+              <h3>
+                About Student Points
+              </h3>
+               <h2> What is Super Coin in Flipkart?</h2>
+                 Student Points is a unique reward program by SDM COllege, where students get rewarded for winning and participating in different events. Student points is distributed both for individual and team events. However, the best student title will be awarded to the student according to the student points he or she collected.
+<br>
+<br>
+<h2> How to earn Flipkart Super Coins?</h2>
+In order to earn Student points, all you have to do is to register to the different events which is organised by different departments. After registering, you need to participate in the events you have registered.<br><br>
+<b><h3>Note :</h3></b>
+  <p>*Student points will be distributed for every participation of student in the event.</p>
+  <p>*Student points will be distributed after the result have been announced          </p>
+            </div>
+          </div>
+        </div>
+      </div>
+  </section>
+
+  <!-- end about section -->
+<?php
+include("footer.php");
+?>
