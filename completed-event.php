a07f720ca0dce97e0a8c071dce1be89cd10538a3
saji37 <sajiththomas162631@gmail.com>
2021-10-04 10:39:14 +0530

    Update completed-event.php

--- completed-event.php
+++ completed-event.php
@@ -11,15 +11,21 @@ include("header.php");
           Events
         </h3>
         <p>
-          Completed Events
+          Recently Completed Events
         </p>
       </div>
       <?php
+      $count=1;
+        $sqlres = "SELECT * FROM  event_result";
+        $qsqlres = mysqli_query($con,$sqlres);
         $sqlview = "SELECT * FROM  event where event_date_time < CURDATE()";
         $qsqlview = mysqli_query($con,$sqlview);
         while($rsview = mysqli_fetch_array($qsqlview))
         {
-          ?>
+          if($count<=6)
+          {
+         ?>
+     <div class="boxd">
       <div class="event_container">
         <div class="box">
           <div class="img-box">
@@ -44,15 +50,99 @@ include("header.php");
             </h3>
           </div>
       <div>
+        <?php
+        $sqlres = "SELECT * FROM  event_result where event_id=" .$rsview['event_id'];
+        $qsqlres = mysqli_query($con,$sqlres);
+             if($rsview['event_id'] == $rsres['event_id'])
+              {
+          ?>
       <a href="event_result_report.php?event_id=<?php echo $rsview['event_id']; ?>" class="btn btn-success">View Result</a>
       (<?php echo $rsview['event_participation_type'];?> Event)
+      <?php 
+          }
+      else
+      {
+        ?>
+        <button class="button-3" role="button" >Result will be added shortly</button>
+
+      <?php } ?>
+
       </div>
-        </div>
+      </div>
+    </div>
+  </div>
         <?php
+    
+        $count=$count+1;
+      }
       }
         ?>
     </div>
   </section>
+  <style>
+.boxd {
+  background: white;
+}
+
+.boxd:hover {
+  background-color: #febfb9;
+  cursor: pointer;
+  -webkit-transition: background-color 2s ease-out;
+  -moz-transition: background-color 2s ease-out;
+  -o-transition: background-color 2s ease-out;
+  transition: background-color 2s ease-out;
+}
+.button-3 {
+  appearance: none;
+  background-color: #e64242;
+  border: 5px solid rgba(27, 31, 35, .15);
+  border-radius: 6px;
+  box-shadow: rgba(27, 31, 35, .1) 0 1px 0;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-family: -apple-system,system-ui,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 20px;
+  padding: inherit;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: middle;
+  white-space: nowrap;
+}
+
+.button-3:focus:not(:focus-visible):not(.focus-visible) {
+  box-shadow: none;
+  outline: none;
+}
+
+.button-3:hover {
+  background-color: #2c974b;
+}
+
+.button-3:focus {
+  box-shadow: rgba(46, 164, 79, .4) 0 0 0 3px;
+  outline: none;
+}
+
+.button-3:disabled {
+  background-color: #94d3a2;
+  border-color: rgba(27, 31, 35, .1);
+  color: rgba(255, 255, 255, .8);
+  cursor: default;
+}
+
+.button-3:active {
+  background-color: #298e46;
+  box-shadow: rgba(20, 70, 32, .2) 0 1px 0 inset;
+}
+</style>
 
   <!-- end event section -->
 <?php
